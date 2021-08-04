72539b2ea74cb31a6f9ed200b007e9784f7dcc7a
wayangalihpratama <wgprtm@gmail.com>
2021-08-04 16:28:11 +0800

    [#3] Enhance introduction page

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,17 @@
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const icon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+const Loading = (
+  <Spin
+    indicator={icon}
+    style={{
+      margin: "auto",
+      width: "100%",
+      marginTop: "15%",
+      marginBottom: "30%",
+    }}
+  />
+);
+
+export default Loading;
--- frontend/src/pages/Introduction.jsx
+++ frontend/src/pages/Introduction.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Row, Col, Image } from "antd";
 
 import "./introduction.scss";
 
+import Loading from "../components/Loading";
+
+import { UIStore } from "../data/store";
+
 const Introduction = ({ history }) => {
+  useEffect(() => {
+    UIStore.update((s) => {
+      s.page = "introduction";
+    });
+  }, []);
+
   return (
     <Row justify="center">
       <Col>
-        <div className="title-wrapper">
+        <div className="title-wrapper" data-aos="fade-up">
           <h1>Income Performance Dashboard</h1>
           <p>Improving SmallHolder Farmer Resilience with Living Income</p>
         </div>
-        <div className="content-wrapper odd">
-          <Row align="middle">
-            <Col md={8}>
-              <Image
-                width={300}
-                src="https://placeimg.com/640/480/nature/grayscale"
-              />
+        <div className="content-wrapper odd" data-aos="fade-up">
+          <Row align="middle" justify="center" gutter={[24, 24]}>
+            <Col span={7}>
+              <Image height={300} src="/introduction/smallholder-farming.png" />
             </Col>
-            <Col md={16}>
+            <Col span={14}>
               <h3>Smallholder Farming</h3>
               <p>
                 Smallholder farmers are crucial in the effort to end hunger and
@@ -35,9 +42,9 @@ const Introduction = ({ history }) => {
             </Col>
           </Row>
         </div>
-        <div className="content-wrapper">
-          <Row align="middle">
-            <Col md={16}>
+        <div className="content-wrapper even bg-white" data-aos="fade-up">
+          <Row align="middle" justify="center" gutter={[24, 24]}>
+            <Col span={14}>
               <h3>Living Income</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -52,23 +59,17 @@ const Introduction = ({ history }) => {
                 ullamcorper nulla.
               </p>
             </Col>
-            <Col md={8}>
-              <Image
-                width={300}
-                src="https://placeimg.com/640/480/nature/grayscale"
-              />
+            <Col span={7}>
+              <Image height={300} src="/introduction/smallholder-farming.png" />
             </Col>
           </Row>
         </div>
-        <div className="content-wrapper odd">
-          <Row align="middle">
-            <Col md={8}>
-              <Image
-                width={300}
-                src="https://placeimg.com/640/480/nature/grayscale"
-              />
+        <div className="content-wrapper odd" data-aos="fade-up">
+          <Row align="middle" justify="center" gutter={[24, 24]}>
+            <Col span={7}>
+              <Image height={300} src="/introduction/smallholder-farming.png" />
             </Col>
-            <Col md={16}>
+            <Col span={14}>
               <h3>Measuring the living income gap</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -82,6 +83,161 @@ const Introduction = ({ history }) => {
                 malesuada scelerisque interdum sed purus. Praesent ut
                 ullamcorper nulla.
               </p>
+            </Col>
+          </Row>
+        </div>
+        <div className="content-wrapper even meta-wrapper" data-aos="fade-up">
+          <Row
+            className="meta-right"
+            align="middle"
+            justify="center"
+            gutter={[24, 24]}
+          >
+            <Col span={7}>
+              <h3>Focus crop revenues</h3>
+              <p>
+                Revenues from the focus crop are calculated by multiplying the
+                price, yield and farm size
+              </p>
+            </Col>
+            <Col span={14}>
+              <Row justify="space-around" align="middle" className="meta-body">
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/revenue.png"
+                  />
+                  <h4>Revenue ($)</h4>
+                </Col>
+                <Col span={1}>=</Col>
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/farm-size.png"
+                  />
+                  <h4>Farm size (ha)</h4>
+                </Col>
+                <Col span={1}>X</Col>
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/yield.png"
+                  />
+                  <h4>Yield (kg/ha)</h4>
+                </Col>
+                <Col span={1}>X</Col>
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/price.png"
+                  />
+                  <h4>Price ($/kg)</h4>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row
+            className="meta-left"
+            align="middle"
+            justify="center"
+            gutter={[24, 24]}
+          >
+            <Col span={8}>
+              <Row justify="space-around" align="middle" className="meta-body">
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/production.png"
+                  />
+                  <h4>Production costs ($/ha)</h4>
+                </Col>
+                <Col span={1}>X</Col>
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/farm-size.png"
+                  />
+                  <h4>Farm size (ha)</h4>
+                </Col>
+                <Col span={1}>=</Col>
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/cost.png"
+                  />
+                  <h4>Production cost ($)</h4>
+                </Col>
+              </Row>
+            </Col>
+            <Col span={13}>
+              <h3>Focus crop production costs</h3>
+              <p>
+                The focus crop production costs are calculated by multiplying
+                the farm size and production costs ($/ha). Production costs
+                capture at a minimum two types of costs. Other costs that can be
+                considered are equipment costs, interest costs for loans that
+                are used for focus crop-related purposes or processing costs (if
+                applicable).
+              </p>
+            </Col>
+          </Row>
+        </div>
+        <div
+          className="content-wrapper odd bg-white meta-wrapper"
+          data-aos="fade-up"
+        >
+          <Row
+            className="meta-right"
+            align="middle"
+            justify="center"
+            gutter={[24, 24]}
+          >
+            <Col span={12}>
+              <Row justify="space-around" align="middle" className="meta-body">
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/net-income.png"
+                  />
+                  <h4>Net-income focus crop ($)</h4>
+                </Col>
+                <Col span={1}>=</Col>
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/revenue.png"
+                  />
+                  <h4>Revenues ($)</h4>
+                </Col>
+                <Col span={1}>-</Col>
+                <Col span={3}>
+                  <Image
+                    className="meta-img"
+                    preview={false}
+                    height={50}
+                    src="/introduction/production.png"
+                  />
+                  <h4>Production cost ($)</h4>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </div>
