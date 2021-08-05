62950e3e54983d1b8e934de8f4e7b4c67b6e8a05
dedenbangkit <mail@dedenbangkit.com>
2021-08-05 08:09:12 +0700

    [#2] Change docker-compose CI environment

--- db/docker-entrypoint-initdb.d/000-init.sql
+++ db/docker-entrypoint-initdb.d/000-init.sql
@@ -6,10 +6,3 @@ WITH OWNER = ipd
      ENCODING = 'UTF8'
      LC_COLLATE = 'en_US.UTF-8'
      LC_CTYPE = 'en_US.UTF-8';
-
-CREATE DATABASE idh_ipd_test
-WITH OWNER = ipd
-     TEMPLATE = template0
-     ENCODING = 'UTF8'
-     LC_COLLATE = 'en_US.UTF-8'
-     LC_CTYPE = 'en_US.UTF-8';
