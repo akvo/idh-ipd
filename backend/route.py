229b212572fcbc7f40a522ff1b1da81510182861
dedenbangkit <mail@dedenbangkit.com>
2021-08-06 16:58:34 +0700

    [#15] Add tags to Swagger

--- backend/route.py
+++ backend/route.py
@@ -27,17 +27,10 @@ def get_session():
         session.close()
 
 
-@routes.get("/health-check")
-def health_check():
-    return "OK"
-
-
-@routes.get("/")
-def read_main():
-    return "OK"
-
-
-@routes.post("/user/", response_model=UserBase, summary="add new user")
+@routes.post("/user/",
+             response_model=UserBase,
+             summary="add new user",
+             tags=["User"])
 def add_user(email: str,
              role: UserRole,
              session: Session = Depends(get_session)):
@@ -45,7 +38,10 @@ def add_user(email: str,
     return user
 
 
-@routes.get("/user/", response_model=List[UserBase], summary="get all users")
+@routes.get("/user/",
+            response_model=List[UserBase],
+            summary="get all users",
+            tags=["User"])
 def get_user(skip: int = 0,
              limit: int = 100,
              session: Session = Depends(get_session)):
@@ -55,7 +51,8 @@ def get_user(skip: int = 0,
 
 @routes.get("/user/{id:path}",
             response_model=UserBase,
-            summary="get user detail")
+            summary="get user detail",
+            tags=["User"])
 def get_user_by_id(id: int, session: Session = Depends(get_session)):
     user = crud_user.get_user_by_id(session=session, id=id)
     if user is None:
@@ -65,7 +62,8 @@ def get_user_by_id(id: int, session: Session = Depends(get_session)):
 
 @routes.post("/country/",
              response_model=CountryBase,
-             summary="add new country")
+             summary="add new country",
+             tags=["Country"])
 def add_country(name: str,
                 code: Optional[str] = None,
                 session: Session = Depends(get_session)):
@@ -75,7 +73,8 @@ def add_country(name: str,
 
 @routes.get("/country/",
             response_model=List[CountryBase],
-            summary="get all countries")
+            summary="get all countries",
+            tags=["Country"])
 def get_country(skip: int = 0,
                 limit: int = 100,
                 session: Session = Depends(get_session)):
@@ -85,7 +84,8 @@ def get_country(skip: int = 0,
 
 @routes.get("/country/{id:path}",
             response_model=CountryBase,
-            summary="get country detail")
+            summary="get country detail",
+            tags=["Country"])
 def get_country_by_id(id: int, session: Session = Depends(get_session)):
     country = crud_country.get_country_by_id(session=session, id=id)
     if country is None:
@@ -93,13 +93,19 @@ def get_country_by_id(id: int, session: Session = Depends(get_session)):
     return country.serialize
 
 
-@routes.post("/crop/", response_model=CropBase, summary="add new crop")
+@routes.post("/crop/",
+             response_model=CropBase,
+             summary="add new crop",
+             tags=["Crop"])
 def add_crop(name: str, session: Session = Depends(get_session)):
     crop = crud_crop.add_crop(session=session, name=name)
     return crop
 
 
-@routes.get("/crop/", response_model=List[CropBase], summary="get all crops")
+@routes.get("/crop/",
+            response_model=List[CropBase],
+            summary="get all crops",
+            tags=["Crop"])
 def get_crop(skip: int = 0,
              limit: int = 100,
              session: Session = Depends(get_session)):
@@ -109,7 +115,8 @@ def get_crop(skip: int = 0,
 
 @routes.get("/crop/{id:path}",
             response_model=CropBase,
-            summary="get crop detail")
+            summary="get crop detail",
+            tags=["Crop"])
 def get_crop_by_id(id: int, session: Session = Depends(get_session)):
     crop = crud_crop.get_crop_by_id(session=session, id=id)
     if crop is None:
@@ -119,7 +126,8 @@ def get_crop_by_id(id: int, session: Session = Depends(get_session)):
 
 @routes.post("/company/",
              response_model=CompanyBase,
-             summary="add new company")
+             summary="add new company",
+             tags=["Company"])
 def add_company(name: str,
                 country: int,
                 crop: int,
@@ -150,7 +158,8 @@ def add_company(name: str,
 
 @routes.get("/company/",
             response_model=List[CompanyBase],
-            summary="get all companies")
+            summary="get all companies",
+            tags=["Company"])
 def get_company(skip: int = 0,
                 limit: int = 100,
                 session: Session = Depends(get_session)):
@@ -160,7 +169,8 @@ def get_company(skip: int = 0,
 
 @routes.get("/company/{id:path}",
             response_model=CompanyBase,
-            summary="get company detail")
+            summary="get company detail",
+            tags=["Company"])
 def get_company_by_id(id: int, session: Session = Depends(get_session)):
     company = crud_company.get_company_by_id(session=session, id=id)
     if company is None:
@@ -170,7 +180,8 @@ def get_company_by_id(id: int, session: Session = Depends(get_session)):
 
 @routes.post("/driver-income/",
              response_model=DriverIncomeBase,
-             summary="add new driver income")
+             summary="add new driver income",
+             tags=["Driver Income"])
 def add_driver_income(country: int,
                       crop: int,
                       status: DriverIncomeStatus,
@@ -209,7 +220,8 @@ def add_driver_income(country: int,
 
 @routes.get("/driver-income",
             response_model=List[DriverIncomeBase],
-            summary="get all driver income")
+            summary="get all driver income",
+            tags=["Driver Income"])
 def get_driver_income(skip: int = 0,
                       limit: int = 100,
                       session: Session = Depends(get_session)):
@@ -221,9 +233,11 @@ def get_driver_income(skip: int = 0,
 
 @routes.get("/driver-income/{crop:path}/{country:path}",
             response_model=DriverIncomeBase,
-            summary="get driver income")
-def get_driver_income_detail(
-        crop: int, country: int, session: Session = Depends(get_session)):
+            summary="get driver income",
+            tags=["Driver Income"])
+def get_driver_income_detail(crop: int,
+                             country: int,
+                             session: Session = Depends(get_session)):
     driver_income = crud_driver_income.get_driver_income_by_crop_and_country(
         session=session,
         crop=crop,
@@ -236,7 +250,8 @@ def get_driver_income_detail(
 
 @routes.get("/country-company",
             response_model=List[CountryCompanyBase],
-            summary="get country and the company list")
+            summary="get country and the company list",
+            tags=["Country", "Company"])
 def get_country_company(session: Session = Depends(get_session)):
     country = crud_country.get_company(session=session)
     return [i.serialize for i in country]
@@ -244,7 +259,20 @@ def get_country_company(session: Session = Depends(get_session)):
 
 @routes.get("/crop-company",
             response_model=List[CropCompanyBase],
-            summary="get crop and the company list")
-def get_country_company(session: Session = Depends(get_session)):
+            summary="get crop and the company list",
+            tags=["Crop", "Company"])
+def get_crop_company(session: Session = Depends(get_session)):
     crop = crud_crop.get_company(session=session)
     return [i.serialize for i in crop]
+
+
+@routes.get("/", tags=["Dev"])
+def read_main():
+    return "OK"
+
+
+@routes.get("/health-check", tags=["Dev"])
+def health_check():
+    return "OK"
+
+
