8df9419bff89177d690b6f542009f08ac406bd06
dedenbangkit <mail@dedenbangkit.com>
2021-08-11 15:28:09 +0700

    [#23] Replace font url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./font.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
