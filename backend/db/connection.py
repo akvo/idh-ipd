6fd2781d961c430bc4a0b7fb39d0b7f2c3ffa11f
dedenbangkit <mail@dedenbangkit.com>
2021-08-21 09:02:05 +0700

    [#38] Raise HTTPException

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import sessionmaker
 
 def get_db_url():
     TESTING = environ.get("TESTING")
-    print(TESTING)
     DATABASE_URL = environ.get("DATABASE_URL")
     DB_URL = f"{DATABASE_URL}_test" if TESTING else DATABASE_URL
     return DB_URL
--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -2,7 +2,7 @@ import sys
 import jwt
 import pytest
 from httpx import AsyncClient
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from middleware import verify_token
 from datetime import datetime, timedelta
 from sqlalchemy.orm import Session
@@ -10,28 +10,37 @@ from db import crud_user
 
 sys.path.append("..")
 
-exp_date = (datetime.now() + timedelta(days=30)).timestamp()
-account = {
-    "email": "support@akvo.org",
-    "name": "Akvo Support",
-    "exp": exp_date,
-    "email_verified": True
-}
-token = jwt.encode(account, "secret", algorithm="HS256")
-decoded = jwt.decode(token, "secret", algorithms=["HS256"])
+
+class Acc:
+    def __init__(self, verified):
+        self.exp_date = (datetime.now() + timedelta(days=30)).timestamp()
+        self.data = {
+            "email": "support@akvo.org",
+            "name": "Akvo Support",
+            "exp": self.exp_date,
+            "email_verified": verified
+        }
+        self.token = jwt.encode(self.data, "secret", algorithm="HS256")
+        self.decoded = jwt.decode(self.token, "secret", algorithms=["HS256"])
 
 
 class TestAuthorizationSetup:
     def test_token_verification(self):
-        assert token != ""
-        assert decoded == account
-        assert True if verify_token(decoded) else False
+        account = Acc(True)
+        assert account.token != ""
+        assert account.decoded == account.data
+        assert True if verify_token(account.decoded) else False
+        account = Acc(False)
+        with pytest.raises(HTTPException):
+            verify_token(account.decoded)
 
     @pytest.mark.asyncio
     async def test_user_get_registered(self, app: FastAPI, session: Session,
                                        client: AsyncClient) -> None:
-        res = await client.post(app.url_path_for("user:register"),
-                                headers={"Authorization": f"Bearer {token}"})
+        account = Acc(True)
+        res = await client.post(
+            app.url_path_for("user:register"),
+            headers={"Authorization": f"Bearer {account.token}"})
         assert res.status_code == 200
         user = crud_user.get_user_by_id(session=session, id=1)
         res = res.json()
